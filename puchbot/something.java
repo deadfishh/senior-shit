fd8f33552dcf1890c5d8342bb63442a47dffbb76
dead fish <111475197+deadfishh@users.noreply.github.com>
2022-10-12 15:44:23 -0400

    Create something.java

---
+++ puchbot/something.java
@@ -0,0 +1,3 @@
+// this is just a placeholder until i can get my shit together and upload the million different fiels for puchbot
+// ChadBot.java, ChadBot2.java, qanon.java, etc
+// gotta peer review those bitches
